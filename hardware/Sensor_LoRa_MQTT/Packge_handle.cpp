9c19d6e4745c1ad7b48a2b94b106542cf16ebe69
includeLuisFerreira <prgluisfelipe@gmail.com>
2026-05-28 08:14:41 -0300

    (chore) Excluindo arquivos de Hardware, imagem e Readme para a implementação da nova arquitetura

--- hardware/Sensor_LoRa_MQTT/Packge_handle.cpp
+++
@@ -1,8 +0,0 @@
-#include "Package_handle.h"
-
-#include <stdint.h>
-#include <stdbool.h>
-
-bool is_valid_package_size(uint8_t packageSize) {
-    return (packageSize == DATA_SENSOR_PK) || (packageSize == CHANGE_PARAM_PK);
-}
